f6025a1450230465f733a7cc48b89141ee8aee32
Kagata Hayato <z00s600184@rfg.rfgad.ricoh.com>
2020-12-09 16:53:55 +0900

    commit

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -18,13 +18,13 @@ it('hoby_Box', () => {
 it('favorite_Box', () => {
   cy.visit('/')
   cy.get('.favorite > h2').contains('猫が好き！！')
-  cy.get('p').eq(5).contains('画像はそれぞれ実際に撮影したものです！')
-  cy.get('p').eq(6).contains('画像をクリックすると地図にジャンプ')
-  cy.get('.favorite-link__text').eq(0).contains('xxxx')
-  cy.get('.favorite-link__text').eq(1).contains('xxxx')
+  cy.get('.favorite > .favorite__texts > p').eq(0).contains('画像はそれぞれ実際に撮影したものです！')
+  cy.get('.favorite > .favorite__texts > p').eq(1).contains('画像をクリックすると地図にジャンプ')
+  cy.get('.favorite > .favorite-link > div[name="cafe"]').contains('xxxx')
+  cy.get('.favorite > .favorite-link > div[name="road"]').contains('xxxx')
   
-  cy.get('a').eq(0).should('have.attr','href','https://www.google.com/maps/place/%E7%8C%AB%E3%82%AB%E3%83%95%E3%82%A7%E3%82%AF%E3%83%AD/@35.4577386,133.359276,17z/data=!3m1!4b1!4m5!3m4!1s0x3556587b32d24adf:0x5666160dd6110b92!8m2!3d35.4577386!4d133.3614647')
-  cy.get('a').eq(1).should('have.attr','href','https://www.google.com/maps?q=%E6%B0%B4%E6%9C%A8%E3%81%97%E3%81%92%E3%82%8B%E3%83%AD%E3%83%BC%E3%83%89&rlz=1C1QABZ_jaJP895JP895&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiMs83R9JTsAhVP05QKHcNvDckQ_AUoAXoECBwQAw')
+  cy.get('.favorite > .favorite-link > a[name="cafe-map"]')should('have.attr','href','https://www.google.com/maps/place/%E7%8C%AB%E3%82%AB%E3%83%95%E3%82%A7%E3%82%AF%E3%83%AD/@35.4577386,133.359276,17z/data=!3m1!4b1!4m5!3m4!1s0x3556587b32d24adf:0x5666160dd6110b92!8m2!3d35.4577386!4d133.3614647')
+  cy.get('.favorite > .favorite-link > a[name="road-map"]')should('have.attr','href','https://www.google.com/maps?q=%E6%B0%B4%E6%9C%A8%E3%81%97%E3%81%92%E3%82%8B%E3%83%AD%E3%83%BC%E3%83%89&rlz=1C1QABZ_jaJP895JP895&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiMs83R9JTsAhVP05QKHcNvDckQ_AUoAXoECBwQAw')
 })
 
 it('slider_box', () => {
