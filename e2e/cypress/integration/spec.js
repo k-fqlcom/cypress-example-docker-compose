4c84bad258bc2614e5d4d05709a5a4b4ba78b3e7
Kagata Hayato <z00s600184@rfg.rfgad.ricoh.com>
2020-12-09 15:32:17 +0900

    commit

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -2,7 +2,7 @@ it('introduction_Box', () => {
   cy.visit('/')
   cy.get('h1').contains('山田太郎の自己紹介')
   cy.get('[class="self-introduction__picture"]').should('have.attr','src','image/kagata.jpg')
-  cy.get('.self-introduction > self-introduction__text > p[name="name"]').contains('Name : 山田太郎')
+  cy.get('.self-introduction > .self-introduction__text > p[name="name"]').contains('Name : 山田太郎')
   cy.get('p').contains('Age : xx歳')
   cy.get('p').contains('From : xx県')
   cy.get('p').contains('TEL : XXX-XXX')
